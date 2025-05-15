c22ff45f9529839985ce300eec1d4d2378af9bee
Jay Cee <jakechalmers86@gmail.com>
2025-05-15 14:54:46 +0800

    Add (Microsoft Learn) Challenge Solution

--- BasicStringFormatting/program.cs
+++ BasicStringFormatting/program.cs
@@ -57,7 +57,7 @@ using System;
         Console.WriteLine($"{russianMessage}\n\t{englishFilePath}{projectName}{russianExpression}{dataTxt}");
         */
 
-        //Cleaned up Solution
+        //My Cleaned up Solution
         string projectName = "ACME";
         string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
@@ -68,6 +68,14 @@ using System;
         Console.WriteLine($"View English output:\n\t{filePath}{projectName}{dataFile}\n");
         Console.WriteLine($"{russianMessage}\n\t{filePath}{projectName}{locale}{dataFile}");
 
+        //Microsoft Learn Solution - Even Cleaner! 
+        string projectName = "ACME";
+        string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+        Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+        string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+        string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+        Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 
 
