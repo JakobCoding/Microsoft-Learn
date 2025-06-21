6c80215db12f59569bef17b8157ae0072947110b
Jake <jakechalmers86@gmail.com>
2025-06-21 19:27:13 +0800

    Exercise: Complete Initilize an array

--- BasicStringFormatting/program.cs
+++ BasicStringFormatting/program.cs
@@ -6,25 +6,19 @@ using System;
     static void Main()
     {
         // Initialize an array with 3 elements
-        string[] orderIDs = [ "A123", "B456", "C789" ];
+        string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
 
-
-        /*
-        * Decclare a new Array of strings to hold 3 elements
-        string[] fraudulentOrderIDs = new string[3];
-
-        fraudulentOrderIDs[0] = "A123";
-        fraudulentOrderIDs[1] = "B456";
-        fraudulentOrderIDs[2] = "C789";
-    
         Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
         Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
         Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
         fraudulentOrderIDs[0] = "F000";
+
         Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-        */
+        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+
 
 
 
